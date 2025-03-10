EdgeGATLayer gradient flow is fixed

src/alphagrad/GNN/GNN_test.py
@@ -4,6 +4,9 @@ import unittest
 from parameterized import parameterized
 import inspect
 
+import equinox as eqx
+import optax
+
 import jax
 import jax.lax as lax
 import jax.numpy as jnp
@@ -26,7 +29,7 @@ from alphagrad.transformer.models import PPOModelGNN
 # jax.config.update("jax_disable_jit", True)
 
 key_ = jrand.PRNGKey(42)
-key1, key2 = jrand.split(key_)
+key1, key2 , key_normal= jrand.split(key_, 3)
 
 F = RobotArm_6DOF
 
@@ -41,8 +44,19 @@ dense_graph = make_graph(F, *xs)
 sparse_graph = graph_sparsify(dense_graph)
 sparse_graph = add_self_edges_fn(sparse_graph)
 
-model = PPOModelGNN(4, 5, 1, [16, 16], [16, 16], key1)
+model = PPOModelGNN(4, 5, 1, [16, 16], [16, 16], sparse_graph.n_node, key1)
 
-logits, value = model(sparse_graph, key2)
+
+target_dist = jax.nn.softmax(jrand.normal(key_normal, (120,)))
+
+def loss_fn(model):
+    prob_dist, value = model(sparse_graph)
+    loss = jnp.sum(prob_dist - target_dist)
+    loss += value[0]
+    return loss
+
+for i in range(100):
+    grads = eqx.filter_grad(loss_fn)(model)
+    print(jax.tree_util.tree_leaves(grads))
 
 print("Done")

src/alphagrad/GNN/graph_network.py
@@ -7,42 +7,15 @@ import equinox as eqx
 from equinox import AbstractVar
 from typing import Callable, Sequence
 from abc import ABC, abstractmethod
+from chex import PRNGKey
 
 from alphagrad.GNN.graph_utils import apply_edge_sparsity_embedding, apply_node_op_type_embedding
 
-# Abstract base class for a graph network layer
-class graphNetworkLayer(eqx.Module, ABC):
-
-    @abstractmethod
-    def layer_fn(self):
-        pass
-
-    @abstractmethod
-    def __call__(self, graph: jr.GraphsTuple) -> jr.GraphsTuple:
-        pass
-
-
-class graphNetwork(eqx.Module, ABC):
-    '''
-    Works as an Actor and outputs which node to eliminate next as a mask over all nodes.
-    '''
-
-    @abstractmethod
-    def __call__(self, graph: jr.GraphsTuple) -> jnp.ndarray:
-        '''
-        Args: 
-            graph (jr.GraphsTuple): A graph represented as a GraphsTuple object whose nodes will be eliminated.
-
-        Returns:
-            jax.numpy.ndarray: Mask of the nodes that will be eliminated.
-        '''
-        pass
-
-
 # Implementation of EdgeGATLayer:
-class EdgeGATLayer(graphNetworkLayer):
+class EdgeGATLayer(eqx.Module):
     edge_update: eqx.nn.Linear
     node_update: eqx.nn.Linear
+    attention_logit: eqx.nn.Linear
     # attention_logit: eqx.nn.Linear
 
     """
@@ -50,44 +23,52 @@ class EdgeGATLayer(graphNetworkLayer):
     """
 
     def __init__(self, edge_feature_size, node_feature_size, transformed_edge_feature_size, transformed_node_feature_size, keys: jrand.PRNGKey):
+        super().__init__()
 
         edge_key, node_key, attn_key = jrand.split(keys, 3)
-
         aggregated_edge_feature_size = edge_feature_size + 2 * node_feature_size # since we are concatenating the sender and receiver node features with current edge feature.
         aggregated_node_feature_size = node_feature_size + 2 * transformed_edge_feature_size
-        # attention_logit_in_feature_size = transformed_edge_feature_size + 2 * transformed_node_feature_size
+        attention_logit_in_feature_size = 2 * node_feature_size
         self.edge_update = eqx.nn.Linear(aggregated_edge_feature_size, transformed_edge_feature_size, key=edge_key)
         self.node_update = eqx.nn.Linear(aggregated_node_feature_size, transformed_node_feature_size, key=node_key)
-        # self.attention_logit = eqx.nn.Linear(attention_logit_in_feature_size, 1, key=attn_key)
+        # self.attention_logit = lambda k, v:  jnp.dot(k, v) / jnp.max(jnp.array([1., jnp.sqrt(len(k))]))
+        self.attention_logit = eqx.nn.Linear(attention_logit_in_feature_size, 1, key=attn_key)
+    
+    """
+    The following functions are executed in the order:
+        1) update_edge_fn
+        2) attention_logit_fn
+        3) attention_reduce_fn
+        4) update_node_fn
+    """
 
     # Preprocess the edge features to create new feature embeddings. 
     # These will be used to calculate the attention scores.
-    def update_edge_fn(self, edge_features, sender_node_features, receiver_node_features, global_features):
+    def update_edge_fn(self, edge_features, sender_node_features, receiver_node_features, global_features=None):
         aggregated_features = jnp.concatenate((edge_features, sender_node_features, receiver_node_features), axis=-1) # batch of aggregated features
         # vectorize for each edge:
-        out = jax.vmap(self.edge_update)(aggregated_features)
-        updated_edge_features = jnn.gelu(out)
+        out_fn = jax.vmap(lambda x: self.edge_update(x))
+        out = out_fn(aggregated_features)
+        updated_edge_features = jnn.relu(out)
 
         return updated_edge_features
     
-    def update_node_fn(self, current_node_features, outgoing_edge_features, incoming_edge_features, global_features):
-        aggregated_features = jnp.concatenate((current_node_features, outgoing_edge_features, incoming_edge_features), axis=-1) # batch of aggregated features
+    def update_node_fn(self, current_node_features, updated_outgoing_edge_features, updated_incoming_edge_features, global_features=None):
+        aggregated_features = jnp.concatenate((current_node_features, updated_outgoing_edge_features, updated_incoming_edge_features), axis=-1) # batch of aggregated features
         # vectorize for each node:
-        out = jax.vmap(self.node_update)(aggregated_features)
-        updated_node_features = jnn.gelu(out)
+        out_fn = jax.vmap(lambda x: self.node_update(x))
+        out = out_fn(aggregated_features)
+        updated_node_features = jnn.relu(out)
 
         return updated_node_features
     
-    def attention_logit_fn(self, updated_edge_features, updated_sender_node_features, updated_receiver_node_features, global_features):
-        # aggregated_features = jnp.concatenate((updated_edge_features, updated_sender_node_features, updated_receiver_node_features), axis=-1) # batch of aggregated features
-        # vectorize for each edge:
-        # out = jax.vmap(self.attention_logit)(aggregated_features)
-
+    def attention_logit_fn(self, updated_edge_features, sender_node_features, receiver_node_features, global_features=None):
         # Attention scores for edges are computed using the nodes connected to that edge. 
         # If node i features are more important to node j features, the edge between i and j 
         # will have a higher attention score.
-
-        attention_logits = jax.vmap(lambda k, v:  jnp.dot(k, v) / jnp.sqrt(len(k)), in_axes=(0, 0))(updated_sender_node_features, updated_receiver_node_features)
+        features = jnp.concatenate((sender_node_features, receiver_node_features), axis=-1)
+        logit_fn = jax.vmap(lambda x: self.attention_logit(x))
+        attention_logits = logit_fn(features)
 
         return attention_logits
     
@@ -97,30 +78,22 @@ class EdgeGATLayer(graphNetworkLayer):
         weighted_edge_features = jax.vmap(lambda k, v: k * v, in_axes=(0, 0))(edge_features, attention_weights)
 
         return weighted_edge_features
-    
-    def layer_fn(self):
-        gat_layer = jr.GraphNetGAT(
+
+
+    def __call__(self, graph: jr.GraphsTuple, key: PRNGKey) -> jr.GraphsTuple:
+        
+        return jr.GraphNetGAT(
             update_edge_fn=self.update_edge_fn,
             update_node_fn=self.update_node_fn,
             attention_logit_fn=self.attention_logit_fn,
             attention_reduce_fn=self.attention_reduce_fn
-        )
-
-        return gat_layer
-
-    def __call__(self, graph: jr.GraphsTuple) -> jr.GraphsTuple:
-        
-        return self.layer_fn()(graph)
+        )(graph)
         
 
 
-class EdgeGATNetwork(graphNetwork):
+class EdgeGATNetwork(eqx.Module):
     sparsity_embedding: eqx.nn.Embedding
-    gat_layers: Sequence[EdgeGATLayer]
-    output_layer: eqx.nn.Linear
-    
-    edge_feature_shapes: Sequence[int] = eqx.field(static=True)
-    node_feature_shapes: Sequence[int] = eqx.field(static=True)
+    gat_layers: eqx.nn.Sequential
 
     def __init__(
             self,
@@ -131,46 +104,41 @@ class EdgeGATNetwork(graphNetwork):
             node_feature_shapes: Sequence[int],
             key: jrand.PRNGKey
     ):
+        
+        super().__init__()
 
-        self.edge_feature_shapes = edge_feature_shapes
-        self.node_feature_shapes = node_feature_shapes
-
-        embed_key_1, embed_key_2, out_key = jrand.split(key, 3)
+        embed_key_1, gat_key, out_key = jrand.split(key, 3)
 
         self.sparsity_embedding = eqx.nn.Embedding(21, edge_sparsity_embedding_size, key=embed_key_1)
-        self.output_layer = eqx.nn.Linear(node_feature_shapes[-1], 1, key=out_key) # to generate the mask
-
-        keys = jrand.split(key, len(edge_feature_shapes)+1)
-        self.gat_layers = []
-
+        gat_keys = jrand.split(gat_key, len(edge_feature_shapes)+1)
         first_layer_edge_feature_shape = edge_sparsity_embedding_size + init_edge_feature_shape - 1
 
-        self.gat_layers.append(
-            EdgeGATLayer(first_layer_edge_feature_shape, init_node_feature_shape, edge_feature_shapes[0], node_feature_shapes[0], keys[0])
-        )
-
-        for i in range(1, len(edge_feature_shapes)):
-            self.gat_layers.append(EdgeGATLayer(edge_feature_shapes[i-1], node_feature_shapes[i-1], edge_feature_shapes[i], node_feature_shapes[i], keys[i]))
+        self.gat_layers = eqx.nn.Sequential([
+            EdgeGATLayer(first_layer_edge_feature_shape, init_node_feature_shape, edge_feature_shapes[0], node_feature_shapes[0], gat_keys[0]),
+            *[EdgeGATLayer(edge_feature_shapes[i], node_feature_shapes[i], edge_feature_shapes[i+1], node_feature_shapes[i+1], gat_keys[i+1]) 
+            for i in range(len(edge_feature_shapes)-1)]
+        ])
 
         return
     
     def apply_sparsity_embeddings(self, graph: jr.GraphsTuple) -> jr.GraphsTuple:
         return apply_edge_sparsity_embedding(self.sparsity_embedding, graph)
     
-    @jax.jit
+    @eqx.filter_jit
     def __call__(self, graph: jr.GraphsTuple) -> jnp.ndarray:
-
-
-        
+        """Forward pass of the GNN.
+        Args:
+            graph: GraphsTuple containing the graph data.
+        Returns:
+            nodes: Elimination probabilities for each node.
+        """
         if graph.nodes.ndim == 1:
             graph = graph._replace(nodes=jnp.expand_dims(graph.nodes, axis=-1))
-        mask = graph.nodes
+
         graph = self.apply_sparsity_embeddings(graph)
 
-        for layer_ in self.gat_layers:
-            graph = layer_(graph)
+        graph = self.gat_layers(graph)
 
-        nodes = jax.vmap(self.output_layer)(graph.nodes)
-        nodes = jnp.squeeze(nodes)
+        node_features = jnp.squeeze(graph.nodes)
 
-        return jnp.where(jnp.squeeze(mask) > 0., -jnp.inf, nodes) # Already eliminated nodes are -jnp.inf so that after softmax they have 0 elimination probability.
+        return node_features

src/alphagrad/ppo/vertex_ppo_GNN.py
@@ -106,11 +106,16 @@ model = PPOModelGNN(
     edge_sparsity_embedding_size=4,
     init_edge_feature_shape=5,
     init_node_feature_shape=1,
-    edge_feature_shapes=[16, 32],
-    node_feature_shapes=[16, 32],
+    edge_feature_shapes=[24, 48],
+    node_feature_shapes=[24, 48],
     num_nodes=sparse_graph.n_node[0],
     key=key
 )
+"""
+Edge feature shape: 5
+Node feature shape: 1
+Embedded edge sparsity shape: 4
+"""
 
 
 init_fn = jnn.initializers.orthogonal(jnp.sqrt(2))
@@ -172,13 +177,11 @@ def get_num_clipping_triggers(ratio):
     return jnp.sum(_ratio)
 
 
-@partial(jax.vmap, in_axes=(None, 0, 0, 0))
-def get_log_probs_and_value(network, state, action, key):
-    mask = 1. - jnp.squeeze(state.nodes)
-    logits, value = network(state, key=key)
-    prob_dist = jnn.softmax(logits, axis=-1, where=mask)
-
+@partial(jax.vmap, in_axes=(None, 0, 0))
+def get_log_probs_and_value(network, state, action):
+    prob_dist, value = network(state)
     log_prob = jnp.log(prob_dist[action] + 1e-7)
+
     return log_prob, prob_dist, value, entropy(prob_dist)
 
 
@@ -305,8 +308,7 @@ def rollout_fn(network, rollout_length, init_carry, key):
         # mask = 1. - state.at[1, 0, :].get()
         mask = 1. - jnp.squeeze(state.nodes)
 
-        action_logits, state_value_estimate = network(state, key=net_key)
-        prob_dist = jnn.softmax(action_logits, axis=-1, where=mask)
+        prob_dist, state_value_estimate = network(state)
                 
         distribution = distrax.Categorical(probs=prob_dist)
         action = distribution.sample(seed=act_key)
@@ -314,7 +316,7 @@ def rollout_fn(network, rollout_length, init_carry, key):
         next_state, reward, done = step_sparse(state, action)
         discount = 1.
 
-        next_logits, next_state_value_estimate = network(next_state, key=next_net_key)
+        next_logits, next_state_value_estimate = network(next_state)
         
         new_sample = (
             state, 
@@ -327,7 +329,7 @@ def rollout_fn(network, rollout_length, init_carry, key):
             prob_dist, 
             jnp.array([discount]))
          
-        return next_state, (new_sample, prob_dist, action_logits)
+        return next_state, (new_sample)
     
     return lax.scan(step_fn, init_carry, keys)
 
@@ -346,8 +348,8 @@ def loss(network, trajectories, keys):
     returns = jnp.squeeze(trajectories[10])
     advantages = jnp.squeeze(trajectories[11])
     
-    log_probs, prob_dist, values, entropies = get_log_probs_and_value(network, state, actions, keys)
-    _, _, next_values, _ = get_log_probs_and_value(network, next_state, actions, keys)
+    log_probs, prob_dist, values, entropies = get_log_probs_and_value(network, state, actions)
+    _, _, next_values, _ = get_log_probs_and_value(network, next_state, actions)
     norm_adv = (advantages - jnp.mean(advantages)) / (jnp.std(advantages) + 1e-7)
     
     # Losses
@@ -377,8 +379,7 @@ def loss(network, trajectories, keys):
 
 @eqx.filter_jit
 def train_agent(network, opt_state, trajectories, keys):  
-    # with jax.disable_jit():
-    grads, metrics = eqx.filter_grad(loss, has_aux=True)(network, trajectories, keys)   
+    grads, metrics = eqx.filter_grad(loss, has_aux=True)(network, trajectories, keys)  
     updates, opt_state = optim.update(grads, opt_state)
     network = eqx.apply_updates(network, updates)
     return network, opt_state, metrics
@@ -388,7 +389,6 @@ def train_agent(network, opt_state, trajectories, keys):
 def test_agent(network, rollout_length, keys):
     env_carry = init_carry_sparse(keys)
     _, trajectories = rollout_fn(network, rollout_length, env_carry, keys)
-    trajectories, prob_dist, action_logits = trajectories
     returns = get_returns(trajectories)
     best_return = jnp.max(returns[:, 0], axis=-1)
     idx = jnp.argmax(returns[:, 0], axis=-1)
@@ -421,7 +421,6 @@ for episode in pbar:
     env_carry = jax.jit(init_carry_sparse)(keys)
     
     env_carry, trajectories = rollout_fn(model, ROLLOUT_LENGTH, env_carry, keys)
-    trajectories, prob_dist, action_logits = trajectories 
 
     trajectories = get_advantages(trajectories)
 

src/alphagrad/transformer/models.py
@@ -173,8 +173,9 @@ class PPOModelGNN(eqx.Module):
     and value networks.
     """
 
-    policy_net: EdgeGATNetwork
-    value_net: EdgeGATNetwork
+    GAT_net: eqx.Module
+    value_net: eqx.nn.Sequential
+    policy_net: eqx.nn.Sequential
     
     def __init__(
             self,
@@ -183,11 +184,14 @@ class PPOModelGNN(eqx.Module):
             init_node_feature_shape: int,
             edge_feature_shapes: Sequence[int],
             node_feature_shapes: Sequence[int],
+            num_nodes: int,
             key: PRNGKey
     ) -> None:
         super().__init__()
         policy_key, value_key = jrand.split(key, 2)
-        self.policy_net = EdgeGATNetwork(
+        val1_ley, val2_key, val3_key = jrand.split(value_key, 3)
+
+        self.GAT_net = EdgeGATNetwork(
                 edge_sparsity_embedding_size, 
                 init_edge_feature_shape, 
                 init_node_feature_shape, 
@@ -195,27 +199,41 @@ class PPOModelGNN(eqx.Module):
                 node_feature_shapes, 
                 policy_key
         )
-        self.value_net = EdgeGATNetwork(
-                edge_sparsity_embedding_size, 
-                init_edge_feature_shape, 
-                init_node_feature_shape, 
-                edge_feature_shapes, 
-                node_feature_shapes, 
-                value_key
-        )
+
+        out_node_feature_shape = node_feature_shapes[-1]
+
+        ravel_node_features_shape = num_nodes * out_node_feature_shape
+
+        self.policy_net = eqx.nn.Sequential([
+            eqx.nn.Linear(out_node_feature_shape, 64, key=val1_ley),
+            eqx.nn.Linear(64, 32, key=val2_key),
+            eqx.nn.Linear(32, 1, key=val3_key)
+        ])
+
+        self.value_net = eqx.nn.Sequential([
+            eqx.nn.Linear(ravel_node_features_shape, 128, key=val1_ley),
+            eqx.nn.Linear(128, 64, key=val2_key),
+            eqx.nn.Linear(64, 1, key=val3_key)
+        ])
+
     
-    def __call__(self, sparse_graph: GraphsTuple, key: PRNGKey) -> Array:
+    def __call__(self, sparse_graph: GraphsTuple) -> Array:
 
         elimination_mask = sparse_graph.nodes
-        elimination_mask = jnp.squeeze(elimination_mask)
+        elimination_mask = 1. - jnp.squeeze(elimination_mask)
 
-        action_logits_unmasked = self.policy_net(sparse_graph)
-        action_logits = jnp.where(elimination_mask > 0, -jnp.inf, action_logits_unmasked)
+        node_features = self.GAT_net(sparse_graph)
 
-        state_values = self.value_net(sparse_graph)
-        state_value_estimate = jnp.mean(state_values)
+        node_elimination_vals = jax.vmap(self.policy_net, in_axes=(0))(node_features)
+        node_elimination_vals = jnp.squeeze(node_elimination_vals)
+        node_elimination_logits = jnp.where(elimination_mask == 0., -jnp.inf, node_elimination_vals)
+        action_prob_dist = jnn.softmax(node_elimination_logits, axis=0)
 
-        return action_logits, state_value_estimate
+
+        ravel_node_features = node_features.ravel()
+        state_value_estimate = self.value_net(ravel_node_features)
+
+        return action_prob_dist, state_value_estimate
 
 class AlphaZeroModel(eqx.Module):
     embedding: eqx.nn.Conv2d